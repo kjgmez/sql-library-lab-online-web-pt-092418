d2a1e7968970f30186c66d3e3f35414ffeb887f5
Kevin J Gomez <kjgmez1@gmail.com>
2018-12-12 15:32:18 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE sub_genres (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE uthors (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, characters_id INTEGER, books_id INTEGER);
